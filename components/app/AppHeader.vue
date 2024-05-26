9d09b8a75abf94e84d881d197e2eb1f15abdfd9d
Hassan Kadhim <hassan57905@gmail.com>
2024-05-26 14:51:00 +0300

    up

---
+++ components/app/AppHeader.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="flex h-14 items-center border-b px-4 lg:h-[60px] lg:px-6">
+    <a href="/" class="flex items-center gap-2 font-semibold">
+      <Package2 class="h-6 w-6" />
+      <span class="">داشبوردي</span>
+    </a>
+    <Button variant="outline" size="icon" class="ms-auto h-8 w-8">
+      <Bell class="h-4 w-4" />
+      <span class="sr-only">Toggle notifications</span>
+    </Button>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { Bell, CircleUser, Home, LineChart, Menu, Package, Package2, Search, ShoppingCart, Users } from 'lucide-vue-next'
+</script>
+
+<style></style>
---
+++ components/app/AppSidebar.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="flex-1">
+    <nav class="grid items-start px-2 text-sm font-medium lg:px-4">
+      <NuxtLink class="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary" >
+         <Home class="h-4 w-4" />
+        لوحة التحكم
+         <Badge
+          class="ms-auto flex h-6 w-6 shrink-0 items-center justify-center rounded-full"
+        >
+          6
+        </Badge>
+      </NuxtLink>
+    </nav>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { Bell, CircleUser, Home, LineChart, Menu, Package, Package2, Search, ShoppingCart, Users } from 'lucide-vue-next'
+</script>
+
+<style></style>
